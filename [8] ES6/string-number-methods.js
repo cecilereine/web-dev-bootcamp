1828b81a900fa1ef772c9f1defd44b782dca2dc1
Cecile <cecile@xurpas.rocks>
2018-04-20 18:17:43 +0800

    + string and number methods

---
+++ [8] ES6/string-number-methods.js
@@ -0,0 +1,26 @@
+let theString = 'Hello, my name is Brad and I love JavaScript';
+
+// startsWith()
+// endsWith()
+// includes()
+
+// console.log(theString.startsWith('Hello')); // true
+// console.log(theString.endsWith('JavaScript')); // true
+// console.log(thiString.includes('love')); // false
+
+
+
+// Hex
+// console.log(0xFF);
+// Binary
+// console.log(0b101011);
+// Octal
+// console.log(0o543);
+
+
+// Number Methods
+ // console.log(Number.isFinite(3));
+
+ // console.log(Number.isNan(NaN));
+
+ // console.log(Number.isInteger(-2));
