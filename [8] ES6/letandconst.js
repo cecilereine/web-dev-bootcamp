5fa9d5ee29074ec2f331446b0784ab9dc5b9876d
Cecile <cecile@xurpas.rocks>
2018-04-20 17:56:30 +0800

    + initial file for Learning ES6 course

---
+++ [8] ES6/letandconst.js
@@ -0,0 +1,42 @@
+// var a = 'Test1';
+// let b = 'Test2';
+
+// function testVar(){
+// 	var a = 30;
+// 	if(true){
+// 		var a = 50;
+// 		console.log(a);
+// 	}
+
+// 	console.log(a);
+// }
+// result: 50 50
+
+
+// function testLet(){
+// 	let a = 30;
+// 	if(true){
+// 		let a = 50;
+// 		console.log(a);
+// 	}
+
+// 	console.log(a);
+// }
+// result: 50 30
+
+// for(let i = 0; i < 10; i++){
+// 	console.log(i);
+// }
+
+// console.log(i);
+
+
+
+let colors = []; // const
+
+colors.push('red');
+colors.push('blue');
+
+colors = 'Green';
+
+console.log(colors);
