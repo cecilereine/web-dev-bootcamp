99decd18cfbd9b3eba89c552701023afb2388ce5
Cecile <cecile@xurpas.rocks>
2018-04-25 11:04:19 +0800

    * implemented comments section for posts

--- Practical Exam - Javascript/posts/posts-main.js
+++ Practical Exam - Javascript/posts/posts-main.js
@@ -9,15 +9,46 @@ $(document).ready(function(){
     }).done(function(data) {       
         //console.log(data);
         $.map(data, function(post, i){
-            console.log(post.userId + " " + userid + (post.userid == userid));
             if(post.userId == userid){            
                 let textToAppend =
                     `<h3>${post.title}</h3>
                     <p>${post.body}</p>
+                    <a href="#" onclick="showComments(${post.id})">COMMENTS</a>
+                    <ul class="comments" id="post${post.id}_comments"></ul>
+                    <hr>
                     `;
                 $('#posts').append(textToAppend);
             }
         });
     });
-
 });
+
+function showComments(postid){
+   event.preventDefault();
+    console.log('show comments clicked');
+    
+    $.ajax({
+        method: 'GET',
+        url: 'http://jsonplaceholder.typicode.com/comments',
+        dataType: 'json'
+    }).done(function(data) {    
+        console.log('got json')
+        $('ul#post'+postid+'_comments').empty();   
+        $.map(data, function(comment, i){
+            
+            if(postid == comment.postId){
+                let textToAppend = 
+                    `<li>
+                        <h4>Name: ${comment.name}</h4>
+                        <h5>Email: ${comment.email}</h5>
+                        <p>${comment.body}</p>
+                    </li>
+                    `;
+
+                
+                $('ul#post'+postid+'_comments').append(textToAppend);
+            }
+        });
+    });
+
+}
